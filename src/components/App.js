b142682cd9ca2fcdc74589be57aebdeec960a984
Simon Mercier <simon.mercier@ubisoft.com>
2019-01-25 15:54:54 -0500

    added user state to Header thanks to redux <3

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './Header'
+import Header from '../containers/Header'
 import MainContent from '../containers/MainContent'
 
 export default function App () {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Header () {
+export default function Header (props) {
   return (
-    <div>
-      <h1>PLAYER 360?</h1>
+    <div style={{ display: 'flex', width: '100%' }}>
+      <h1 style={{ width: '70%' }}>PLAYER 360?</h1>
+      <p style={{ alignSelf: 'center', width: '30%' }}>
+        Connected user: {props.user.username}
+      </p>
     </div>
   )
 }
